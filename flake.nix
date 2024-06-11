4b8c63fa7fe260370ab1bd1c54b5bc0670382334
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-06-11 21:25:07 +0800

    style(flake): format

--- flake.nix
+++ flake.nix
@@ -7,27 +7,27 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       pythonEnv = shell: (pkgs.buildFHSUserEnv {
-          name = "python-env";
-          targetPkgs = pkgs: (with pkgs; [
-            python3
-            python3Packages.pip
-            pipenv
+        name = "python-env";
+        targetPkgs = pkgs: (with pkgs; [
+          python3
+          python3Packages.pip
+          pipenv
 
-            # Support binary wheels from PyPI
-            pythonManylinuxPackages.manylinux2014Package
+          # Support binary wheels from PyPI
+          pythonManylinuxPackages.manylinux2014Package
 
-            # Enable building from sdists
-            cmake
-            ninja
-            gcc
-            pre-commit
-          ]);
-          runScript = "${pkgs.writeShellScriptBin "runScript" ''
+          # Enable building from sdists
+          cmake
+          ninja
+          gcc
+          pre-commit
+        ]);
+        runScript = "${pkgs.writeShellScriptBin "runScript" ''
             export PIPENV_VENV_IN_PROJECT=1
             pipenv install
             PIPENV_SHELL=${shell} pipenv shell
           ''}/bin/runScript";
-        }).env;
+      }).env;
     in
     {
       devShell = pythonEnv "fish";
